ec50ce3a109ba7c943de9fbec504fb3a24fc2bc9
shvydak <35421423+shvydak@users.noreply.github.com>
2023-03-02 21:01:17 +0200

    Second save

---
+++ src/test/java/Start.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Start {
+
+    WebDriver wd;
+
+    @Test
+    public void start(){
+        wd=new ChromeDriver();
+//        wd.get("https://www.google.com/");
+        wd.navigate().to("https://www.google.com/");
+        wd.navigate().refresh();
+
+        wd.close(); // close only one tab, if there is just only tab - it closes the browser
+        wd.quit(); // close the browser with all opened tabs
+    }
+
+}
