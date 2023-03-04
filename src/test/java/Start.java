99db87c974f32c0d2e99f755056513547977daa3
shvydak <35421423+shvydak@users.noreply.github.com>
2023-03-04 15:30:59 +0200

    update link to telran

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -10,7 +10,7 @@ public class Start {
     public void start(){
         wd=new ChromeDriver();
 //        wd.get("https://www.google.com/");
-        wd.navigate().to("https://www.google.com/");
+        wd.navigate().to("https://telranedu.web.app/home");
 
 //        wd.close(); // close only one tab, if there is just only tab - it closes the browser
         wd.quit(); // close the browser with all opened tabs
