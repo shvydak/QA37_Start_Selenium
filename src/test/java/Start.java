b98ea2553b3b6b551a8f9c4caeff85c24cd241f8
shvydak <y.shvydak@gmail.com>
2023-03-04 15:41:07 +0200

    Update from MacBook

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -9,8 +9,8 @@ public class Start {
     @Test
     public void start(){
         wd=new ChromeDriver();
-//        wd.get("https://www.google.com/");
-        wd.navigate().to("https://telranedu.web.app/home");
+        wd.get("https://www.google.com/");
+//        wd.navigate().to("https://telranedu.web.app/home");
 
 //        wd.close(); // close only one tab, if there is just only tab - it closes the browser
         wd.quit(); // close the browser with all opened tabs
