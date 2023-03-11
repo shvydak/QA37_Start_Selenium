99e04d7a2120eb4071193e1ee0e162a6f04a98dd
shvydak <y.shvydak@gmail.com>
2023-03-11 17:28:13 +0200

    HW1728

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
@@ -21,16 +22,17 @@ public class HomeWork {
     @Test
     public void cssLocators() {
         // by tag name
-        wd.findElement(By.tagName("body"));
-        wd.findElement(By.tagName("div"));
-        wd.findElement(By.tagName("h1"));
-        wd.findElement(By.tagName("form"));
-        wd.findElement(By.tagName("input"));
-        wd.findElement(By.tagName("button"));
+        WebElement byTagName0 = wd.findElement(By.tagName("body"));
+        WebElement byTagName1 = wd.findElement(By.tagName("div"));
+        WebElement byTagName2 = wd.findElement(By.tagName("h1"));
+        WebElement byTagName3 = wd.findElement(By.tagName("form"));
+        WebElement byTagName4 = wd.findElement(By.tagName("input"));
+        WebElement byTagName5 = wd.findElement(By.tagName("button"));
 
         // by class
-        wd.findElement(By.className("container"));
-        wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        WebElement byClassName0 = wd.findElement(By.className("container"));
+        WebElement byClassName1 = wd.findElement(By.className("navbar-component_nav__1X_4m"));
+//        WebElement byClassName2 = wd.findElement(By.className(""));
         //By id
 
         // by attribute
